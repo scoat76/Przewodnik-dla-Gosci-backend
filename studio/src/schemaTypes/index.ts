de464dc351b25e063511fb8b6a686f6cc1969f75
scoat76 <33258885+scoat76@users.noreply.github.com>
2025-08-18 16:38:43 +0200

    Update index.ts

--- studio/src/schemaTypes/index.ts
+++ studio/src/schemaTypes/index.ts
@@ -1,24 +1,5 @@
-import {person} from './documents/person'
-import {page} from './documents/page'
-import {post} from './documents/post'
-import {callToAction} from './objects/callToAction'
-import {infoSection} from './objects/infoSection'
-import {settings} from './singletons/settings'
-import {link} from './objects/link'
-import {blockContent} from './objects/blockContent'
+import generalInfo from './generalInfo'
+import faq from './faq'
+import pointOfInterest from './pointOfInterest'
 
-// Export an array of all the schema types.  This is used in the Sanity Studio configuration. https://www.sanity.io/docs/schema-types
-
-export const schemaTypes = [
-  // Singletons
-  settings,
-  // Documents
-  page,
-  post,
-  person,
-  // Objects
-  blockContent,
-  infoSection,
-  callToAction,
-  link,
-]
+export const schemaTypes = [generalInfo, faq, pointOfInterest]
